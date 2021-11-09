cccfb62f4f229d4c11995dee1b395f4547e6662e
danielrbimo <hantorobimo@gmail.com>
2021-11-08 19:38:16 -0500

    print 10 to console

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,7 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-((() => {
-
-  console.log('Hello, world!');
-
-})());
+// Immediately Invoked Function Expression to limit access to our
+// variables and prevent
+d3.csv("data/VAR_Incidents_19_20.csv").then((data) => {
+for (let i = 0; i < 10; i++){
+  console.log(data[i]);
+}
+});
